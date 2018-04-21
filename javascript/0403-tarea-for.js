cd344f47732e978e28a3993b79a6e49897014e38
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-04-21 18:49:26 -0500

    Tarea FizzBuzz

---
+++ javascript/0403-tarea-for.js
@@ -0,0 +1,10 @@
+/**
+ * Escribe un programa que use console.log para imprimir todos 
+ * los números del 1 al 100, con las siguientes excepciones.
+ * - Para numeros divisibles por 3 - imprime la palabra "Fizz"
+ * - Para numeros divisibles por 5 - imprime la palabra "Buzz"
+ * 
+ * -- RETO: Si ya terminaste lo anterior, intenta resolver lo
+ * siguiente
+ * - Para numeros divisibles por 3 y 5 - imprime la palabra "FizzBuzz"
+ */
