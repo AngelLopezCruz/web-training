55707bb41e0a98b299f7ea7825bb6a8870b0b4b0
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-04-22 17:04:01 -0500

    add encuentra el mayor

---
+++ javascript/0406-tarea-for-v2.js
@@ -0,0 +1,21 @@
+/**
+ * Descripción
+ * Dado un conjunto de N datos enteros, determina el mayor de ellos.
+ * 
+ * Entrada
+ * En la primera línea el entero N. En la segunda los N enteros separados por espacios.
+ * 
+ * Salida
+ * El número mayor del conjunto
+ * 
+ * Ejemplo:
+ * 
+ * Entrada:
+ * 5
+ * 2 3 50 8 1
+ * 
+ * Salida:
+ * 50
+ * 
+ * https://omegaup.com/arena/problem/Encuentra-el-mayor#problems
+ */
