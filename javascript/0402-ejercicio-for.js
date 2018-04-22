068eec036c5f2e3e4cc3326118663dd57b06008d
unknown <Jorge_ina@outlook.com>
2018-04-21 18:46:22 -0600

    Resuelto piramides

--- javascript/0402-ejercicio-for.js
+++ javascript/0402-ejercicio-for.js
@@ -13,3 +13,18 @@
  * ######
  * #######
  */
+
+
+let rayasPiramidalInicio = 1;
+let rayasPiramidalFin = 70;
+
+for (let i = 1; i <= rayasPiramidalFin; i++) {
+    
+    let linea='';
+
+    for (let j = 1; j <= i; j++) {
+        linea = linea + '&';
+    }
+
+    console.log(linea);
+}
