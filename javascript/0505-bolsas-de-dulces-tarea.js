c2605f121415b096dc499beb2ffdd11675812d5f
unknown <Jorge_ina@outlook.com>
2018-05-02 20:40:13 -0600

    ejercicio 5 resuelto

--- javascript/0505-bolsas-de-dulces-tarea.js
+++ javascript/0505-bolsas-de-dulces-tarea.js
@@ -1,1 +1,18 @@
 // https://omegaup.com/arena/problem/Bolsas-de-dulces#problems
+
+function ventaClandestina(listaDeDulces, listaDePrecios, numerosElementos){
+    let resultado = 0;
+
+    for(let i = 0; i < numerosElementos; i++){
+        resultado = resultado + (listaDeDulces[i] * listaDePrecios[i]);
+    }
+
+    return resultado;
+}
+
+
+let N = 5;
+let tiposDeBolsas = [1,2,3,8,2];
+let preciosDeDulces = [3,5,15,50,31];
+
+console.log(ventaClandestina(tiposDeBolsas,preciosDeDulces,N));
