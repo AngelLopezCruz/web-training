f6e259f7dd2dd7d2908011400363a172d5943e6f
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-05-02 20:50:58 -0500

    lista de dulces con lista de objetos

--- javascript/0505-bolsas-de-dulces-tarea.js
+++ javascript/0505-bolsas-de-dulces-tarea.js
@@ -10,9 +10,27 @@ function ventaClandestina(listaDeDulces, listaDePrecios, numerosElementos){
     return resultado;
 }
 
+function calculaVentas(numElementos, listaDeDulces) {
+    let resultado = 0;
+
+    for (let i = 0; i < numElementos; i++) {
+        resultado += (listaDeDulces[i].tipoDeDulce * listaDeDulces[i].precio); // abreviamos la suma
+    }
+
+    return resultado;
+}
 
 let N = 5;
 let tiposDeBolsas = [1,2,3,8,2];
 let preciosDeDulces = [3,5,15,50,31];
 
-console.log(ventaClandestina(tiposDeBolsas,preciosDeDulces,N));
+let bolsasDeDulces = [
+    { tipoDeDulce: 1, precio: 3 },
+    { tipoDeDulce: 2, precio: 5 },
+    { tipoDeDulce: 3, precio: 15 },
+    { tipoDeDulce: 8, precio: 45 },
+    { tipoDeDulce: 2, precio: 31 }
+];
+
+// console.log(ventaClandestina(tiposDeBolsas,preciosDeDulces,N));
+console.log(calculaVentas(N, bolsasDeDulces));
