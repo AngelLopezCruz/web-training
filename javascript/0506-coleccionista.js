afa1512a7caf89636374d77a6faa051ac05dd5ae
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-05-02 21:18:11 -0500

    se agrega indexOf funciones para arreglos

---
+++ javascript/0506-coleccionista.js
@@ -0,0 +1,32 @@
+// https://omegaup.com/arena/problem/Coleccionista/#problems
+
+// function encontrarFaltante(numElementos, elementosActuales) {
+//   for (let i = 1; i <= N; i++) {
+//     // if (noExisteElementoEnLaLista(i)) {
+//     if (elementosActuales.indexOf(i) < 0) {
+//       return i;
+//     }
+//   }
+// }
+
+function encontrarFaltante(numElementos, elementosActuales) {
+  let sumaDeElementos = 0;
+  let sumaActual = 0;
+
+  for (let i=1; i <=numElementos; i++) {
+    sumaDeElementos += i; //  sumaDeElementos = sumaDeElementos + i;
+  }
+
+  for (let i = 0; i < listaDeNumeros.length; i++) {
+    sumaActual += listaDeNumeros[i];
+  }
+  
+  return sumaDeElementos - sumaActual;
+}
+
+
+
+let N = 5;
+let listaDeNumeros = [5, 2, 4, 1];
+
+console.log(encontrarFaltante(N, listaDeNumeros));
